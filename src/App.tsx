78cc162d643d4b53d7f7c20e9bf2491385ce771b
OlaTobiszewska <aleksandra.tobiszewska08+shEdu@gmail.com>
2020-03-26 11:00:53 +0100

    Add basic button component and theme
    Problems to resolve:
    - emotion ThemeProvider should pass theme to styled component as a prop
    - styled component loses it's default props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.scss';
+import { ThemeProvider } from 'emotion-theming';
+import Button from './components/atoms/Button/Button';
+import { theme } from './common/theme';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Button
+          buttonTitle="some button"
+          onClick={() => {console.log('clicked!')}}
+          color="secondary"
+          className='test-class'
+        />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { theme } from '../../../common/theme'
+import './button.scss';
 
-export const Button: React.StatelessComponent<{}> = () => (
-  <div className="">Button
-  </div>
+type Props = {
+  buttonTitle: string,
+  onClick: () => void,
+  color?: string,
+  className?: string,
+}
+
+const Button = (
+  {
+    buttonTitle,
+    onClick,
+    className,
+  }: Props
+) => (
+  <button
+    className={className}
+    onClick={onClick}
+  >
+    { buttonTitle }
+  </button>
 );
+
+export default styled(Button)`
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: ${({ color = "primary" }) => theme.colors[color]};
+`
