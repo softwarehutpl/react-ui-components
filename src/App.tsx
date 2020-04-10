550a6c6c5139ce942325119176223bed874e04f1
Monika Sudol <monikaasudol@gmail.com>
2020-04-10 10:19:32 +0200

    Add deafult font and trransform styles to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.scss';
 import { ThemeProvider } from 'styled-components';
 import theme from './common/theme';
-import breadcrumbsItems from './common/mocks/breadcrumbsItems';
 import { styleReorder } from './helpers/styleReorder';
 import Breadcrumbs from './components/atoms/Breadcrumbs/Breadcrumbs';
 import DropdownItem from './components/atoms/DropdownItem/DropdownItem';
 import { Dropdown } from './components/molecules/Dropdown/Dropdown';
 import Button from './components/atoms/Button/Button';
 import Input from './components/atoms/Input/Input';
+import './components/atoms/Breadcrumbs/Breadcrumbs.scss';
+
+const items = [{
+  name: 'Home', link: 'link',
+  }, {
+    name: 'Products', link: 'link-2',
+  }, {
+    name: 'Collection', link: 'link-3',
+  }, {
+    name: 'Product Details', link: 'link-4',
+  }];
 
 function App() {
   const [value, setValue] = useState('');
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
     styleReorder();
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (progress !== 100) {
+        setProgress(progress + 10);
+      }
+    }, 500);
+    return () => {clearInterval(interval)};
+  }, [progress])
+
   return (
     <ThemeProvider theme={theme}>
       <Dropdown>
-        <DropdownItem heading disabled>item</DropdownItem>
+        <DropdownItem heading disabled>
+          item
+        </DropdownItem>
         <DropdownItem divider dividerColor="blue" />
         <DropdownItem
           onClick={() => {
@@ -38,24 +60,25 @@ function App() {
           }}
         />
         <Input
-          placeholder='test'
+          placeholder="test"
           value={value}
           showPlaceholderOnFocus
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
-          hoverBackgroundColor='#EEE'
-          focusBackgroundColor='#EEE'
-          transitionEffect='mid'
-          label='test12e'
+          hoverBackgroundColor="#EEE"
+          focusBackgroundColor="#EEE"
+          transitionEffect="mid"
+          label="test12e"
           width={250}
         />
         <Breadcrumbs
-          items={breadcrumbsItems}
+          items={items}
           showOnlyBorderItems
           activeBreadcrumbClassName="active"
           firstBreadcrumbClassName="active"
           itemClassName='breadcrumbsItem'
           wrapperClassName='breadcrumbsWrapper'
           noBorder
+          showBoxShadow
         />
       </div>
     </ThemeProvider>
